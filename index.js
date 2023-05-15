4420a103f9030f0f20fb75245e9c656d2408ffa1
Serpentarius13 <serpentarium13@mail.ru>
2023-05-15 06:07:01 +0300

    message

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require("express");
+
+
+const formidable = require("formidable");
+
+const pdfjs = require("pdfjs-dist");
+
+const app = express();
+
+const fs = require("fs");
+
+const middle = express.urlencoded({
+  extended: false,
+  limit: 10000,
+  parameterLimit: 2,
+});
+
+function toArrayBuffer(buffer) {
+  const arrayBuffer = new ArrayBuffer(buffer.length);
+  const view = new Uint8Array(arrayBuffer);
+  for (let i = 0; i < buffer.length; ++i) {
+    view[i] = buffer[i];
+  }
+  return arrayBuffer;
+}
+
+app.post("/pdf", middle, async (req, response) => {
+  try {
+    const form = formidable({ multiples: true });
+
+    const result = await new Promise((res, rej) => {
+      form.parse(req, async (err, fields, files) => {
+        console.log(files.file.filepath);
+
+        const file = fs.readFileSync(files.file.filepath);
+
+        pdfjs.GlobalWorkerOptions.workerSrc =
+          "pdfjs-dist/legacy/build/pdf.worker";
+
+        const task = pdfjs.getDocument(toArrayBuffer(file));
+
+        const pdf = await task.promise;
+
+        const { numPages } = pdf;
+
+        let textContent = "";
+
+        await Promise.all(
+          Array({ length: numPages }).map(async (el, ix) => {
+            const page = await pdf.getPage(ix + 1);
+
+            const text = await page.getTextContent();
+
+            text.items.forEach((item) => (textContent += `${item.str} \n`));
+          })
+        );
+
+        res(textContent);
+      });
+    });
+
+    response.json({ text: response });
+  } catch (error) {
+    response.status(400).json("Error parsing pdf");
+  }
+});
+
+app.listen(process.env.PORT || 4000, () => {
+  console.log("working");
+});
